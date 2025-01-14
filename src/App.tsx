c428b29800d7bd39d5ca1a814e3db26f02b4dfb6
Prakhar Gupta <prakhargupta3680@gmail.com>
2025-01-15 00:22:16 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,12 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import Player from "./components/Player";
-import { Physics } from "@react-three/rapier";
-import { Suspense, useRef, useEffect } from "react";
-import { Html } from "@react-three/drei";
-import globalState from "./state/global-state";
-import { Button } from "@/components/ui/button";
-import Loading from "./components/Loading";
+import Plane from "./components/Plane";
 
 export default function App() {
-  const {
-    isIntroCompleted,
-    setIsIntroCompleted,
-    checkFullScreen,
-    setCheckFullScreen,
-  } = globalState();
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-  const handleButtonClick = () => {
-    setIsIntroCompleted(true);
-    canvasRef.current?.requestFullscreen?.();
-  };
-
-  useEffect(() => {
-    const handleFullscreenChange = () => {
-      setCheckFullScreen(document.fullscreenElement === canvasRef.current);
-    };
-
-    const canvasElement = canvasRef.current;
-    canvasElement?.addEventListener("fullscreenchange", handleFullscreenChange);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      canvasElement?.removeEventListener(
-        "fullscreenchange",
-        handleFullscreenChange
-      );
-    };
-  }, [setCheckFullScreen]);
-
   return (
-    <Canvas ref={canvasRef} camera={{ position: [0, 0, 5] }}>
-      <Suspense>
-        { !checkFullScreen && isIntroCompleted && <Loading canvasRef={ canvasRef.current } /> }
-        
-        {isIntroCompleted ? (
-          <Physics>
-            <Player />
-          </Physics>
-        ) : (
-          <Html className='r3f-html' center>
-            <div className='grid gap-3'>
-              <p>R3F Bricks</p>
-              <Button onClick={handleButtonClick}>Let's Play</Button>
-            </div>
-          </Html>
-        )}
-      </Suspense>
+    <Canvas style={{ background: "black" }}>
+      <Plane />
+      <OrbitControls />
     </Canvas>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import glsl from "vite-plugin-glsl";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,9 +24,4 @@ export default defineConfig({
       root: "/", // Directory for root imports
     }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
